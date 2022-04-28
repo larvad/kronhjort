3cffc6ca3bb57841259afc3402ffedc188be8ac7
CodingKof <cph-mk378@cphbusiness.dk>
2022-04-28 12:57:14 +0200

    commmit

--- src/main/java/dat/startcode/control/Micki.java
+++ src/main/java/dat/startcode/control/Micki.java
@@ -1,4 +1,13 @@
 package dat.startcode.control;
 
-public class Micki {
+import dat.startcode.model.exceptions.DatabaseException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Micki extends Command {
+    @Override
+    String execute(HttpServletRequest request, HttpServletResponse response) throws DatabaseException {
+        return null;
+    }
 }
---
+++ src/main/webapp/WEB-INF/micki.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: mrkoe
+  Date: 28-04-2022
+  Time: 12:54
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Velkommen til medina</title>
+</head>
+<body>
+
+</body>
+</html>
